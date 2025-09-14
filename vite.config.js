950e61eb5902d64b09cbd70f2709c1ecbb3968a3
Frausino <davirosa8@hotmail.com>
2025-09-14 18:05:18 -0300

    teste2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteCompression(), visualizer({ filename: "stats.html" })],
+  build: { target: "es2020", sourcemap: false, cssCodeSplit: true },
+});
